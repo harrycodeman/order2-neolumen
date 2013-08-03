be704987627dadc04a602960144c8d223479da95
maxnagibator <bob217@mail.ru>
2013-08-04 01:21:09 +0700

    v.0.9.5 сделана сслка на вконтакте, починил баг с неработающими ссылками связанный с перерыванием в scroll.js

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
     $('a').click(function (e) {
-        e.preventDefault();
+
         var a = $(this).attr('href');
+        if(a.substr(0,4)!='http'){
+            e.preventDefault();
         var destination = $(a).offset().top;
         $("html,body").animate({scrollTop: destination}, 300);
-
+        }
     });
 });
